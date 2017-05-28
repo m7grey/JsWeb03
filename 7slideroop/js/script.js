5190cfc65599931114d372ce7c5ab01e997e8d5e
m7grey <m7grey@gmail.com>
2017-05-28 14:03:15 -0400

    Slider2 auto interval.

---
+++ 7slideroop/js/script.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+	var images = document.querySelectorAll('.gallery-1 .photos img');
+	var slider = new Slider(images);
+	document.querySelector('.gallery-1 .buttons .prev').onclick = function () {
+		slider.prev();
+	}
+	document.querySelector('.gallery-1 .buttons .next').onclick = function () {
+		slider.next();
+	}
+	var images2 = document.querySelectorAll('.gallery-2 .photos img');
+	var slider2 = new Slider(images2);
+	setInterval(function () {
+			slider2.next()
+		}
+		, 1000);
+	function Slider(images) {
+		this.images = images;
+		var i = 0;
+		this.prev = function () {
+			this.images[i].classList.remove('showed');
+			i--;
+			if (i < 0) {
+				i = this.images.length - 1;
+			}
+			this.images[i].classList.add('showed');
+		}
+		this.next = function () {
+			this.images[i].classList.remove('showed');
+			i++;
+			if (i >= this.images.length) {
+				i = 0;
+			}
+			this.images[i].classList.add('showed');
+		}
+	}
+}
